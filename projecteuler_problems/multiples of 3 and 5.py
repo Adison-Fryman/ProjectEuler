ad10116dd837eaa1873967b101f31cfc5bda4151
Adison <adison.fryman@gmail.com>
2022-08-17 13:43:18 -0400

    quickly testing my cloning skills

--- projecteuler_problems/multiples of 3 and 5.py
+++ projecteuler_problems/multiples of 3 and 5.py
@@ -1,6 +1,8 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # #we get 3, 5, 6 and 9. The sum of these multiples is 23.
+import time
 
+Import time
 #Find the sum of all the multiples of 3 or 5 below 1000.
 mults_of_3_n_5 = []
 removed_dups= []
@@ -19,4 +21,6 @@ for num in removed_dups:
 print(total_sum)
 
 
+time.sleep(5)
 
+print('thank you for waiting... :)')
